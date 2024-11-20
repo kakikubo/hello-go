a45f41d6d615da4fe1b8178c9dfc0d501c2ce348
kakikubo <kakikubo@gmail.com>
2024-11-21 07:41:13 +0900

    バイト列と文字列を表すstring型とが次のように相互に変換可能

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// バイト列と文字列を表すstring型とが次のように相互に変換可能
+	byteArray := []byte("ASCII")
+	fmt.Printf("%#v\n", byteArray) // []byte{0x41, 0x53, 0x43, 0x49, 0x49}
+	str := string([]byte{0x41, 0x53, 0x43, 0x49, 0x49})
+	fmt.Printf("%#v\n", str) // "ASCII"
+
 }
