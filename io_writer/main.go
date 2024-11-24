e8d543748255cfed0feffba13a386363935993be
kakikubo <kakikubo@gmail.com>
2024-11-25 05:47:07 +0900

    io.MultiWriterのサンプル

--- io_writer/main.go
+++ io_writer/main.go
@@ -54,4 +54,12 @@ func main() {
 		panic(err)
 	}
 	req.Write(conn)
+
+	// io.MultiWriter
+	file, err = os.Create("multiwriter.txt")
+	if err != nil {
+		panic(err)
+	}
+	writer := io.MultiWriter(file, os.Stdout)
+	io.WriteString(writer, "io.MultiWriter example\n")
 }
