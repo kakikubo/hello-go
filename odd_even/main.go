385a25f6bff4483d48ea910d71870d3710a8ff72
kakikubo <kakikubo@gmail.com>
2024-11-28 06:54:08 +0900

    奇数偶数判定とおみくじ

---
+++ odd_even/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d-偶数\n", i)
+		} else {
+			fmt.Printf("%d-奇数\n", i)
+		}
+	}
+	for i := 1; i <= 100; i++ {
+		switch {
+		case i%2 == 0:
+			fmt.Printf("%d-偶数\n", i)
+		default:
+			fmt.Printf("%d-奇数\n", i)
+		}
+	}
+}
