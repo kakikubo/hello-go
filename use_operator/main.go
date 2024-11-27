5dc1a7f20362db39871d59e62df13cef1df2fd22
kakikubo <kakikubo@gmail.com>
2024-11-28 06:35:15 +0900

    【TRY】演算子の利用

---
+++ use_operator/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// 【TRY】演算子の利用
+func main() {
+	// 整数の演算
+	n := 100 + 200
+
+	// 変数を使った演算
+	m := n + 100
+
+	// 文字列の足し算
+	msg := "hoge" + "fuga"
+
+	fmt.Println(n, m, msg)
+}
